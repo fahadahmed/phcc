c0bbb36aae4c335d98431e3f198441363e6bc8af
Fahad Ahmed <contact@fhdamd.dev>
2021-04-17 21:33:01 +1000

    #2 added a paragraph component

---
+++ src/components/Paragraph/Paragraph.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Paragraph = styled.p`
+  font-family: 'Bitter', serif;
+  font-size: 20px;
+  line-height: 32px;
+  color: #00a896;
+`;
+
+export default Paragraph;
--- src/pages/TeamWinRate/index.tsx
+++ src/pages/TeamWinRate/index.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
 import Header from '../../components/Header/Header';
+import Paragraph from '../../components/Paragraph/Paragraph';
 
 function TeamWinRate(): JSX.Element {
   const getCommentary = () => {
     return (
-      <p>
+      <Paragraph>
         The Team Win Rate is the winning percentage of the team over the season across all games.
         This insight shows us how each grade performed overall as well as how did they perform when
         playing home games or away. It also shows how each grade performed when batting first or
         second.
-      </p>
+      </Paragraph>
     );
   };
   return (
-    <>
+    <div style={{ padding: '10px' }}>
       <Header>
         <h1>Team Win Rate</h1>
       </Header>
       {getCommentary()}
-    </>
+    </div>
   );
 }
 
